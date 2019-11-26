71edefb85e6b3e20230070413bc62fee5c60dc46
Kayot88 <kayot88@gmail.com>
2019-11-26 14:10:21 +0200

    basic styles

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>!!!Open up App.js to start working on your app!</Text>
+      <View style={styles.header}>
+        <Text style={styles.boldText}>Hello</Text>
+      </View>
+      <View style={styles.body}>
+        <Text style={styles.boldText}>
+          Lorem ipsum,<Text>Test</Text> dolor sit amet
+        </Text>
+        <Text>Lorem ipsum, dolor sit amet</Text>
+        <Text>Lorem ipsum, dolor sit amet</Text>
+      </View>
     </View>
   );
 }
@@ -12,8 +21,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
   },
+  header: {
+    backgroundColor: "red",
+    padding: 20
+  },
+  boldText: {
+    fontWeight: "bold"
+  },
+  body: {
+    backgroundColor: "yellow",
+    padding: 20
+  }
 });
